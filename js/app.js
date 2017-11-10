1855ce25058620827071452b2a0f566ec710d45e
carlacentenor <carlacentenor@gmail.com>
2017-11-10 11:35:55 -0500

    app.js avance

--- js/app.js
+++ js/app.js
@@ -1,18 +1,41 @@
-var initial = document.querySelector("#c7");
-initial.style.backgroundColor = "#ff1304";
-document.onkeydown= keyListener();
-
 function keyListener(event) {
-  if(event.keyCode ==37){ /**left*/
-    moveBox();
-  }
-  if(event.keyCode ==38){/**top*/
-    moveBox();
-  }
-  if(event.keyCode ==39){/**rigth*/
-    moveBox();
-  }
-  if(event.keyCode ==40){/**bottom*/
-    moveBox();
-  }
+  if (event.keyCode == 37) {
+          document.querySelector("#c6").style.backgroundColor="red";
+          document.querySelector("#c3").style.backgroundColor="white";
+          document.querySelector("#c11").style.backgroundColor="white";
+          document.querySelector("#c8").style.backgroundColor="white";
+      }
+  if (event.keyCode == 38) {
+              document.querySelector("#c3").style.backgroundColor="red";
+              document.querySelector("#c6").style.backgroundColor="white";
+              document.querySelector("#c11").style.backgroundColor="white";
+              document.querySelector("#c8").style.backgroundColor="white";
+      }
+  if (event.keyCode == 39) {
+              document.querySelector("#c8").style.backgroundColor="red";
+              document.querySelector("#c3").style.backgroundColor="white";
+              document.querySelector("#c11").style.backgroundColor="white";
+              document.querySelector("#c6").style.backgroundColor="white";
+          }
+  if (event.keyCode == 40) {
+                  document.querySelector("#c11").style.backgroundColor="red";
+                  document.querySelector("#c3").style.backgroundColor="white";
+                  document.querySelector("#c6").style.backgroundColor="white";
+                  document.querySelector("#c8").style.backgroundColor="white";
+              }
 }
+
+/*
+if(event.keyCode ==37){
+alert("izquierda");
+  }
+if(event.keyCode ==38){
+alert("arriba");
+}
+if(event.keyCode ==39){
+alert("derecha");
+}
+if(event.keyCode ==40){
+  moveBox("abajo");
+}
+*/
